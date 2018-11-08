d72562a132b8f9d29fc5aacb5d1e2c5ef5d00b55
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2018-11-08 18:59:53 -0200

    Backend added

---
+++ backend/src/main/java/br/com/lepsistemas/reactivewhiteboard/ReactiveWhiteBoardApplication.java
@@ -0,0 +1,12 @@
+package br.com.lepsistemas.reactivewhiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWhiteBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWhiteBoardApplication.class, args);
+	}
+}
